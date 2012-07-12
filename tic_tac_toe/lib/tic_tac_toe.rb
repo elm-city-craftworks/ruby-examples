981676897aed4c37fd2b7ce63d5bbb277bee5056
Gregory Brown <gregory.t.brown@gmail.com>
2012-07-12 14:14:30 -0400

    Ignore moves

--- tic_tac_toe/lib/tic_tac_toe.rb
+++ tic_tac_toe/lib/tic_tac_toe.rb
@@ -2,9 +2,14 @@ module TicTacToe
   class Game
     def initialize
       self.players = [:X, :O].cycle
+      self.moves   = Hash.new { |h,k| h[k] = [] }
     end
 
     def move(pos)
+      return if moves.any? { |k,v| v.include?(pos) }
+
+      moves[current_player] << pos
+      
       players.next
     end
 
@@ -14,6 +19,6 @@ module TicTacToe
 
     private
 
-    attr_accessor :players
+    attr_accessor :players, :moves
   end
 end
