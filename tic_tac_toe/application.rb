6e8482ea43083f6cb9498aeb2d4b8e7e9d1c1132
Gregory Brown <gregory.t.brown@gmail.com>
2012-07-13 12:02:00 -0400

    UI spiking

--- tic_tac_toe/application.rb
+++ tic_tac_toe/application.rb
@@ -21,7 +21,9 @@ module TicTacToe
       attr_reader :message
 
       def setup
-        @game = TicTacToe::Game.new
+        @game       = TicTacToe::Game.new
+        @grid       = Grid.new
+
         update_message
       end
 
@@ -30,6 +32,7 @@ module TicTacToe
           input = Ray::TextHelper.convert(char).to_i
 
           if (1..9).include?(input)
+            @grid.update(input, @game.current_player.to_s)
             @game.move(input)
             update_message
           end
@@ -38,10 +41,54 @@ module TicTacToe
  
       def render(win)
         win.draw(text(message, :size => 24, :at => [450,425]))
+
+        @grid.draw(win)
       end
 
       def update_message
         @message = "It's your turn, #{@game.current_player}"
+      end
+    end
+
+    class Grid
+      def initialize
+        counter = 0
+
+        @drawables = [0,1,2].product([0,1,2]).map do |y,x|
+          counter += 1
+          Box.new([100 + x*100, 100 + y*100], counter.to_s)
+        end
+      end
+
+      def update(position, name)
+        box = @drawables[position - 1]
+
+        box.name =  name
+        box.shape.color = Ray::Color.blue
+      end
+
+      def draw(win)
+        @drawables.each { |drawable| drawable.draw(win) }
+      end
+    end
+
+    class Box
+      def initialize(offset, name)
+        @offset = offset
+        @shape    = Ray::Polygon.rectangle([0, 0, 80, 80], Ray::Color.red) 
+        @shape.pos += offset
+
+        @name = name
+      end
+
+      attr_accessor :name
+      attr_reader   :shape
+
+      def draw(win)
+        win.draw(@shape)
+        win.draw(Ray::Text.new(name, :size => 64, 
+                                     :at => @shape.pos + [25, -5], 
+                                     :font => "ibm.ttf"))
       end
     end
   end
